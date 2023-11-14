09be0f86bbd8c475a5546bec0779236c0efd1adc
Sara Mohamed <skyangelsara@gmail.com>
2023-11-14 15:33:21 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -130,9 +130,49 @@ Route::prefix('product')->group(function(){
         return 'Projectors home Page .';
     });
 });
-//======================================
-
-
+//==============Web Structure========================
+Route::get('about',function(){
+    return 'More about our website .';
+});
+Route::get('contact',function(){
+    return 'How can we help you ? ';
+});
+Route::prefix('support')->group(function(){
+    Route::get('/',function(){
+        return 'Welcome to Our Support Page .';
+    });
+    Route::get('chat',function(){
+        return 'Welcome to chat Page .';
+    });
+    Route::get('call',function(){
+        return 'Welcome to call Page .';
+    });
+    Route::get('ticket/{name?}/{num?}',function($name,$num=0){
+        $msg='Weclome '.$name;
+        if($num > 0){
+        return $msg.' <br>You have reserved '.$num.' ticket' ;
+        }
+        return $msg;
+    });
+});
+Route::prefix('training')->group(function(){
+    Route::get('/',function(){
+        return 'Welcome to The Training Page .';
+    });
+    Route::get('hr',function(){
+        return 'Welcome to HR Training Page .';
+    });
+    Route::get('ict',function(){
+        return 'Welcome to ICT Training Page .';
+    });
+    Route::get('marketing',function(){
+        return 'Welcome to Marketing Training Page .';
+    });
+    Route::get('logistics',function(){
+        return 'Welcome to Logistics Training Page .';
+    });
+});
+//=========================================
 
 
 
